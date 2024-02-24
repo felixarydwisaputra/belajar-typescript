8dd33d61bf3df1ed9d74472b19ef75cc5f2b515f
felixary_ <felixarydwisaputra@gmail.com>
2024-02-24 13:53:00 +0800

    belajar typescript - video keenam

---
+++ 06-tipe-objek/index.ts
@@ -0,0 +1,17 @@
+let mahasiswa = {
+  nama: "Feliks", //tipe data harus string
+  umur: 21, //tipe data harus number
+  semester: 7, //tipe data harus number
+  isAktif: true //tipe data harus boolean
+};
+
+mahasiswa = {
+  nama: "Ujangaja",
+  umur: 22,
+  semester: 4,  
+  isAktif: true
+}
+
+//mahasiswa.alamat = "Bali" //tidak dapat menambahkan key value baru
+
+console.log(mahasiswa)
