052754552d057676f68d63c655799185094b612e
felixary_ <felixarydwisaputra@gmail.com>
2024-02-24 13:53:27 +0800

    belajar typescript - video ketujuh

---
+++ 07-tipe-eksplisit/index.ts
@@ -0,0 +1,8 @@
+// tipe deklarasi tipe data string secara tidak langsung (implisit)
+// let nama = "Feliks" 
+
+// tipe deklarasi tipe data string secara langsung (eksplisit)
+let nama: string = "Ujang" 
+let umur: number = 21;
+let isAktif: boolean;
+let kegiatan: string[] = []; // tipe data string of array
